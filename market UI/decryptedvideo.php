8df4df4127c0773f010d638a36ac2c97bdced9cd
HuynhHuuThang <thanghuu.huynh@dxc.com>
2023-07-15 19:26:46 +0700

    update market UI

---
+++ market UI/decryptedvideo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Video Player</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <div class="video-container">
+        <video id="video-player" controls>
+            <source src="video/decryptedvideo/video.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+    </div>
+</body>
+</html>
